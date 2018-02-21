bb39a4971bbb5babbb08c08d2d6bfe28b3a6ac60
Khaled Qasem <khaled.alqerem@gmail.com>
2018-02-21 16:03:33 +0200

    Implement push download notifications

--- src/Iris/DashboardBundle/Controller/PushNotificationsController.php
+++ src/Iris/DashboardBundle/Controller/PushNotificationsController.php
@@ -10,24 +10,30 @@ namespace Iris\DashboardBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use PDO;
 
-
-class PushNotificationsController extends Controller {
+class PushNotificationsController extends Controller
+{
     // (Android)API access key from Google API's Console.
     //new api key
     private static $API_ACCESS_KEY = 'AAAAChyhmPI:APA91bHP89zd5cq-0E2Nhti0Ua9kKibkskRqqky9Vg-KG71pGLtA_-Yk76LJ1QCVd-Bmp5krcxJ10Hrtbx13-392No4n5jcsZlgg6UwilGlUFOWQz8DBjHol0c_su5R-xcG2y-Y3Z81p';
+    private static $TABLET_API_ACCESS_KEY = 'AAAAMwr0Syo:APA91bHVHHBnH-S6GYbII46ux6rrD5Osy6AF3My_XN2OYqe3BO6PrHctFTThQX4-R8satvVdp_ZANn9n9Jr8eQYOp_dMM3_pU7KWP9OJ9Ai7BfemyD5u5PcDjonK-pIDtiUQb-RUxKlh';
+    private static $DONGLE_API_ACCESS_KEY = 'AAAA45uo5ZE:APA91bHmS3bb3baBixg0-GIWYBsfQhSPCLP7G-bO4T4QUrMiwuVRmePQNpqUSgzzu7Y_jahZvPO4CVOrFYdPW5WztX96FLs42n_aVydZO6RzeyqZtJNFInDGj-l_bRAOoEWxovWIDfmq';
     // (iOS) Private key's passphrase.
     private static $passphrase = 'joashp';
     // (Windows Phone 8) The name of our push channel.
     private static $channelName = "joashp";
 
     // Change the above three vriables as per your app.
-    public function __construct() {
+    public function __construct()
+    {
 //        exit('Init function is not allowed');
     }
 
     // Sends Push notification for Android users
-    public function android($data, $reg_id) {
+    public function android($data, $reg_id)
+    {
         $url = 'https://android.googleapis.com/gcm/send';
         $message = array(
             'title' => $data['mtitle'],
@@ -39,7 +45,7 @@ class PushNotificationsController extends Controller {
         );
 
         $headers = array(
-            'Authorization: key=' .self::$API_ACCESS_KEY,
+            'Authorization: key=' . self::$API_ACCESS_KEY,
             'Content-Type: application/json'
         );
 
@@ -66,17 +72,18 @@ class PushNotificationsController extends Controller {
     }
 
     // Sends Push's toast notification for Windows Phone 8 users
-    public function WP($data, $uri) {
+    public function WP($data, $uri)
+    {
         $delay = 2;
-        $msg =  "<?xml version=\"1.0\" encoding=\"utf-8\"?>" .
+        $msg = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" .
             "<wp:Notification xmlns:wp=\"WPNotification\">" .
             "<wp:Toast>" .
-            "<wp:Text1>".htmlspecialchars($data['mtitle'])."</wp:Text1>" .
-            "<wp:Text2>".htmlspecialchars($data['mdesc'])."</wp:Text2>" .
+            "<wp:Text1>" . htmlspecialchars($data['mtitle']) . "</wp:Text1>" .
+            "<wp:Text2>" . htmlspecialchars($data['mdesc']) . "</wp:Text2>" .
             "</wp:Toast>" .
             "</wp:Notification>";
 
-        $sendedheaders =  array(
+        $sendedheaders = array(
             'Content-Type: text/xml',
             'Accept: application/*',
             'X-WindowsPhone-Target: toast',
@@ -86,7 +93,7 @@ class PushNotificationsController extends Controller {
         $response = $this->useCurl($uri, $sendedheaders, $msg);
 
         $result = array();
-        foreach(explode("\n", $response) as $line) {
+        foreach (explode("\n", $response) as $line) {
             $tab = explode(":", $line, 2);
             if (count($tab) == 2)
                 $result[$tab[0]] = trim($tab[1]);
@@ -96,7 +103,8 @@ class PushNotificationsController extends Controller {
     }
 
     // Sends Push notification for iOS users
-    public function iOS($data, $devicetoken) {
+    public function iOS($data, $devicetoken)
+    {
         $deviceToken = $devicetoken;
         $ctx = stream_context_create();
         // ck.pem is your certificate file
@@ -105,7 +113,7 @@ class PushNotificationsController extends Controller {
         // Open a connection to the APNS server
         $fp = stream_socket_client(
             'ssl://gateway.sandbox.push.apple.com:2195', $err,
-            $errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+            $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
         if (!$fp)
             exit("Failed to connect: $err $errstr" . PHP_EOL);
         // Create the payload body
@@ -148,7 +156,8 @@ class PushNotificationsController extends Controller {
 
     // Curl
 //    private function useCurl(&$model, $url, $headers, $fields = null) {
-    private function useCurl($url, $headers, $fields = null) {
+    private function useCurl($url, $headers, $fields = null)
+    {
         // Open connection
         $ch = curl_init();
         if ($url) {
@@ -173,8 +182,107 @@ class PushNotificationsController extends Controller {
             // Close connection
             curl_close($ch);
 
-            return $result;
+//            return $result;
+            return new JsonResponse($result);
         }
+    }
+
+    public function pushDownloadNotificationAction()
+    {
+        $request = $this->get('request');
+        $dbname = $this->container->getParameter('store_database_name');
+        $username = $this->container->getParameter('store_database_user');
+        $password = $this->container->getParameter('store_database_password');
+        $servername = $this->container->getParameter('store_database_host');
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $request = $this->get('request');
+        if ($request->getMethod() == 'POST') {
+            $appID = $request->request->get('AppID');
+            $type = $request->request->get('Type');
+            $userName = $request->request->get('UserName');
+//            $token = $request->request->get('Token');
+
+            $stmt1 = $conn->prepare('Select TOKEN from tokens where UID=? AND TYPE=?');
+            try {
+                $stmt1->execute([$userName, $type]);
+            } catch (\PDOException $e) {
+                $error = 'Operation Aborted ..' . $e->getMessage();
+                $request->getSession()->getFlashBag()->add('danger', $error);
+                return $this->redirect($request->headers->get('referer'));
+            }
+            $token = $stmt1->fetchAll()[0][0];
+
+            $data = array(
+                'AppID' => $appID,
+                'Type' => $type
+            );
+
+            if ($type == "dongle") {
+                return $this->sendToDongle($data, $token);
+            } elseif ($type == "tablet") {
+                return $this->sendToTablet($data, $token);
+            } else {
+                return new JsonResponse(array('token' => $token, 'type' => $type));
+            }
+        }
+        return new JsonResponse(array('token' => "noToken", 'type' => "noType"));
+    }
+
+    // Sends Push notification for Android dongle users
+    public function sendToDongle($data, $token)
+    {
+        $url = 'https://android.googleapis.com/gcm/send';
+        $message = array(
+            'title' => $data['AppID'],
+            'message' => $data['Type'],
+            'subtitle' => '',
+            'tickerText' => '',
+            'msgcnt' => 1,
+            'vibrate' => 1
+        );
+
+        $headers = array(
+            'Authorization: key=' . self::$DONGLE_API_ACCESS_KEY,
+            'Content-Type: application/json'
+        );
+
+//       Send to controller by topic by token
+        $fields = array(
+            'to' => $token,
+            'data' => $message,
+        );
+
+        return $this->useCurl($url, $headers, json_encode($fields));
+    }
+
+    // Sends Push notification for Android dongle users
+    public function sendToTablet($data, $token)
+    {
+        $url = 'https://android.googleapis.com/gcm/send';
+        $message = array(
+            'title' => $data['AppID'],
+            'message' => $data['Type'],
+            'subtitle' => '',
+            'tickerText' => '',
+            'msgcnt' => 1,
+            'vibrate' => 1
+        );
+
+        $headers = array(
+            'Authorization: key=' . self::$TABLET_API_ACCESS_KEY,
+            'Content-Type: application/json'
+        );
+
+//       Send to controller by topic by token
+        $fields = array(
+            'to' => $token,
+            'data' => $message,
+        );
+
+        return $this->useCurl($url, $headers, json_encode($fields));
     }
 
 }
