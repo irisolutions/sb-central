013bd66ed09579b2ed51a2fabcb36afa1c504175
Khaled Qasem <khaled.alqerem@gmail.com>
2018-04-29 12:39:42 +0300

    - Handle delet Token

--- src/Iris/DashboardBundle/Controller/TokensController.php
+++ src/Iris/DashboardBundle/Controller/TokensController.php
@@ -37,7 +37,7 @@ class TokensController extends Controller
             //todo : khaled 21/Feb Try to make one access to DB
             //todo : khaled 21/Feb Check tokens and emails before add new one
 
-            $stmt = $conn->prepare('Insert into  tokens (TOKEN,TYPE,UID) values (?,?,? )');
+            $stmt = $conn->prepare('Insert into  Tokens (Token,Device,UID) values (?,?,? )');
             try {
                 $stmt->execute([$token, $type, $userName]);
             } catch (\PDOException $e) {
@@ -49,6 +49,39 @@ class TokensController extends Controller
             return new JsonResponse(array('token' => $token, 'UID' => $userName, 'type' => $type));
         }
         return new JsonResponse(array('token' => $token, 'UID' => $userName, 'type' => $type));
+    }
+
+    function deleteTokenAction()
+    {
+        $request = $this->get('request');
+        $dbname = $this->container->getParameter('store_database_name');
+        $username = $this->container->getParameter('store_database_user');
+        $password = $this->container->getParameter('store_database_password');
+        $servername = $this->container->getParameter('store_database_host');
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        if ($request->getMethod() == 'POST') {
+            $token = $request->request->get('Token');
+            $userName = $request->request->get('UserName');
+            $type = $request->request->get('Type');
+
+//            $stmt = $conn->prepare('DELETE FROM Tokens WHERE Token = ? AND Device = ? AND UID = ?)');
+            $stmt = $conn->prepare('DELETE FROM Tokens WHERE Token = ?');
+
+            try {
+//                $stmt->execute([$token, $type, $userName]);
+                $stmt->execute([$token]);
+            } catch (\PDOException $e) {
+                $error = 'Operation Aborted ..' . $e->getMessage();
+                $request->getSession()->getFlashBag()->add('danger', $error);
+                return new JsonResponse(array('token' => 'fail', 'error' => $error));
+            }
+            $request->getSession()->getFlashBag()->add('success', "Client Added Successfully");
+            return new JsonResponse(array('token' => $token, 'UID' => $userName, 'type' => $type));
+        }
+        return new JsonResponse(array('token' => 'fail', 'error' => "no post request"));
     }
 
 }
