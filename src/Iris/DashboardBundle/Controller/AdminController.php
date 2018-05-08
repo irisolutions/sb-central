bff1e6390d511faf9bfef2c414cb26075518a53a
iyadjamal <iyadjamal5@gmail.com>
2018-05-08 18:46:59 +0300

    test page

--- src/Iris/DashboardBundle/Controller/AdminController.php
+++ src/Iris/DashboardBundle/Controller/AdminController.php
@@ -18,8 +18,66 @@ class AdminController extends Controller {
     	if( !$context->isGranted('IS_AUTHENTICATED_FULLY') )
     	//	return $this->forward('MelodycodeFossdroidBundle:Homepage:index');
     	return $this->redirect($this->generateUrl('homepage'));
-    	
-    	return new Response('<html><body>Administrator Account Details go here</body></html>');
+        $request=$this->get('request');
+        $ID=$this->getUser()->getUsername();
+        $dbname     = $this->container->getParameter('store_database_name');
+        $username   = $this->container->getParameter('store_database_user');
+        $password   = $this->container->getParameter('store_database_password');
+        $servername = $this->container->getParameter('store_database_host');
+
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if ($request->getMethod() == 'POST')
+        {
+            $newID				= $request->request->get('ID');
+            $UserName 				= $request->request->get('Name');
+            $Password 				= $request->request->get('Password');
+            $Email 				= $request->request->get('Email');
+
+            $stmt = $conn->prepare('UPDATE Administrator SET ID =?, Name=?,Password=?, Email=? WHERE ID=?');
+            try
+            {
+                $stmt->execute([$newID,$UserName,$Password,$Email,$ID]);
+            }        catch (\PDOException $e)
+            {
+                // if something goes wrong we fail
+
+                //throw $e;
+
+
+                $error = 'Operation Aborted ..'.$e->getMessage();
+                print($error);
+                die();
+                $request->getSession()->getFlashBag()->add('danger', $error);
+                return $this->redirect($request->headers->get('referer'));
+
+            }
+
+            return $this->redirect($request->headers->get('referer'));
+
+
+        }
+
+        $stmt = $conn->prepare('select * from Administrator where ID=?');
+        try
+        {
+            $stmt->execute([$ID]);
+        }        catch (\PDOException $e)
+        {
+            // if something goes wrong we fail
+
+            //throw $e;
+
+
+            $error = 'Operation Aborted ..'.$e->getMessage();
+
+            $request->getSession()->getFlashBag()->add('danger', $error);
+            return $this->redirect($request->headers->get('referer'));
+
+        }
+        $userDetail=$stmt->fetch();
+    	return $this->render('DashboardBundle:Account:account.html.twig',array('user'=>$userDetail));
     }
 
  	
