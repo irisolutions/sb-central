cfd577c22e8eb61e0b66b94bcb118f7b658c4cd8
Leena Khaled <leenakhalid393@gmail.com>
2018-05-22 16:43:56 +0300

    add loading ,add link to iris store logo ,improve desc page

--- src/Melodycode/FossdroidBundle/Controller/HomepageController.php
+++ src/Melodycode/FossdroidBundle/Controller/HomepageController.php
@@ -11,12 +11,13 @@ use PDO;
 class HomepageController extends Controller {
 
     public function indexAction() 
-    {   $BundleID = "1";
+    {   $BundleID1 = "1";
         $BundleID2 = "2";
         $BundleID3 = "3";
         $BundleID4 = "4";
         $BundleID5 = "5";
         $BundleID6 = "6";
+        $BundleID7 = "7";
         // ToDo: in case we put the storedb on a separate host, we need to decouple the
         // the db connections here
 
@@ -34,18 +35,18 @@ class HomepageController extends Controller {
         // ToDo: optimize this Subquery/Join later by adding a bundleID field in the maindb.application table. You need to change the parser script to support reading
         // the BundleID(s) from the meta data file for each application
 
-        $stmt = $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
-        $stmt->execute([$BundleID]);
-        $bundle1Applications = $stmt->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
-        $stmt4= $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
-        $stmt4->execute([$BundleID4]);
-        $bundle1Applications4 = $stmt4->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
+        $stmt1 = $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
+        $stmt1->execute([$BundleID1]);
+        $bundle1Applications = $stmt1->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
         $stmt2= $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
         $stmt2->execute([$BundleID2]);
         $bundle1Applications2 = $stmt2->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
         $stmt3= $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
         $stmt3->execute([$BundleID3]);
         $bundle1Applications3 = $stmt3->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
+        $stmt4= $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
+        $stmt4->execute([$BundleID4]);
+        $bundle1Applications4 = $stmt4->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
         $stmt5= $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
         $stmt5->execute([$BundleID5]);
         $bundle1Applications5 = $stmt5->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
@@ -53,51 +54,103 @@ class HomepageController extends Controller {
         $stmt6->execute([$BundleID6]);
         $bundle1Applications6 = $stmt6->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
         //var_dump($applications);
-
-
+        $stmt7 = $conn->prepare('SELECT * FROM maindb.application WHERE id IN (SELECT ApplicationID from storedb.BundleApplication WHERE BundleID= ?)');
+        $stmt7->execute([$BundleID7]);
+        $bundle1Applications7 = $stmt7->fetchAll(PDO::FETCH_CLASS, "Melodycode\FossdroidBundle\Entity\Application");
 
         $conn = null;
-
         $conn = new PDO("mysql:host=$servername;dbname=$dbname2", $username, $password);
-        $stmt = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
-        $stmt->execute([$BundleID]);
+        $stmt1 = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
+        $stmt2 = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
+        $stmt3 = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
+        $stmt4 = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
+        $stmt5 = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
+        $stmt6 = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
+        $stmt7 = $conn->prepare('SELECT * FROM Bundle WHERE ID = ?');
+        $stmt1->execute([$BundleID1]);
         $stmt2->execute([$BundleID2]);
         $stmt3->execute([$BundleID3]);
         $stmt4->execute([$BundleID4]);
         $stmt5->execute([$BundleID5]);
         $stmt6->execute([$BundleID6]);
-        if ($stmt->rowCount() < 1) {
+        $stmt7->execute([$BundleID7]);
+        if ($stmt1->rowCount() < 1) {
             throw $this->createNotFoundException('The bundle does not exist!');
         }
-
-        $bundleinfo = $stmt->fetch();
+        if ($stmt2->rowCount() < 1) {
+            throw $this->createNotFoundException('The bundle does not exist!');
+        }
+        if ($stmt3->rowCount() < 1) {
+            throw $this->createNotFoundException('The bundle does not exist!');
+        }
+        if ($stmt4->rowCount() < 1) {
+            throw $this->createNotFoundException('The bundle does not exist!');
+        }
+        if ($stmt5->rowCount() < 1) {
+            throw $this->createNotFoundException('The bundle does not exist!');
+        }
+        if ($stmt6->rowCount() < 1) {
+            throw $this->createNotFoundException('The bundle does not exist!');
+        }
+        if ($stmt7->rowCount() < 1) {
+            throw $this->createNotFoundException('The bundle does not exist!');
+        }
+        $bundleinfo1 = $stmt1->fetch();
         $bundleinfo2 = $stmt2->fetch();
         $bundleinfo3 = $stmt3->fetch();
+        $bundleinfo4 = $stmt4->fetch();
         $bundleinfo5 = $stmt5->fetch();
         $bundleinfo6 = $stmt6->fetch();
-        $bundleinfo4 = $stmt4->fetch();
+        $bundleinfo7 = $stmt7->fetch();
 //            return $this->render('MelodycodeFossdroidBundle:Bundle:index.html.twig', array(
 //                'bundle' => $bundleinfo,
 //                'bundle1Applications' => $bundle1Applications
 //            ));
+         $slug1="games";
+        $repository_category = $this->getDoctrine()->getRepository('MelodycodeFossdroidBundle:Category');
+        $category1 = $repository_category->findOneBySlug($slug1);
+
+        if (!$category1) {
+            throw $this->createNotFoundException('The category does not exist');
+        }
+
+        $repository_application1 = $this->getDoctrine()->getRepository('MelodycodeFossdroidBundle:Application');
+        $applications3 = $repository_application1->findByPublished(0, 'created_at', $slug1);
+        $slug2="system";
+        $repository_category1 = $this->getDoctrine()->getRepository('MelodycodeFossdroidBundle:Category');
+        $category2 = $repository_category1->findOneBySlug($slug1);
+
+        if (!$category2) {
+            throw $this->createNotFoundException('The category does not exist');
+        }
+
+        $repository_application2 = $this->getDoctrine()->getRepository('MelodycodeFossdroidBundle:Application');
+        $applications2 = $repository_application2->findByPublished(0, 'created_at', $slug2);
+
 
 
         return $this->render('MelodycodeFossdroidBundle:Homepage:index.html.twig', array(
 
-                'bundle' => $bundleinfo,
+                'bundle1' => $bundleinfo1,
                 'bundle2' => $bundleinfo2,
                 'bundle3' => $bundleinfo3,
                 'bundle6' => $bundleinfo6,
                 'bundle5' => $bundleinfo5,
                 'bundle4' => $bundleinfo4,
+                'bundle7' => $bundleinfo7,
+                'bundle1Applications7' => $bundle1Applications7,
                 'bundle1Applications' => $bundle1Applications,
                 'bundle1Applications2' => $bundle1Applications2,
                 'bundle1Applications4' => $bundle1Applications4,
                 'bundle1Applications3' => $bundle1Applications3,
                 'bundle1Applications5' => $bundle1Applications5,
                 'bundle1Applications6' => $bundle1Applications6,
-
-
+                'applications3' => $applications3,
+                'applications2' => $applications2,
+                'category1' => $category1,
+                'category2' => $category2,
+'category1Name' =>$slug1,
+                'category2Name' =>$slug2,
             )
         );
 
