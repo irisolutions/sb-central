8693385a4070cf09aca7775ecbee685b113a5b31
iyadjamal <iyadjamal5@gmail.com>
2018-06-04 13:10:39 +0300

    Refactor Developer1Controller

--- src/Iris/DashboardBundle/Controller/Developer1Controller.php
+++ src/Iris/DashboardBundle/Controller/Developer1Controller.php
@@ -198,101 +198,8 @@ class Developer1Controller extends Controller
         return $this->render('DashboardBundle:Developer:manage-app.html.twig', array(
             'applications' => $applications));
     }
-
-    public function editAppAction($slug)
+    public function get_Store_DB_Object()
     {
-        $context = $this->container->get('security.context');
-
-        if (!$context->isGranted('IS_AUTHENTICATED_FULLY'))
-            return $this->render('DashboardBundle:Homepage:index.html.twig');
-
-        //auth
-        // if we are here then the binary files are in place and the meta data file was created so we insert into the DB
-
-        $request = $this->get('request');
-
-        if ($request->getMethod() == 'POST') {
-
-            if ($this->scriptStillExecuting('update-store.sh')) {
-                //return new Response('<html><body><div>An Add Application Operation is in progress, please try again later</div> <div><input type="button" value="Try Again" onClick="window.history.back()"></div></body> </html>');
-                $request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. Another Add/Update/Delete Application operation is in progress .. Try again in a few seconds');
-                return $this->redirect($request->headers->get('referer'));
-            }
-
-            $app_name = $request->request->get('app-name');
-            $app_bundle = $request->request->get('app-bundle');
-            //$app_controller_binary 	= $request->request->get('app-controller-binary');
-            $app_description = $request->request->get('app-description');
-            //$app_dongle_binary		= $request->request->get('app-dongle-binary');
-            $app_identifier = $request->request->get('app-identifier');
-            $app_payment_model = $request->request->get('app-payment-model');
-            $app_price = $request->request->get('app-price');
-            $app_summary = $request->request->get('app-summary');
-            //$app_version 			= $request->request->get('app-version');
-            $app_category = $request->request->get('app-category');
-            $app_type = $request->request->get('app-type');
-
-            // we check if the two binary files are where they should be other wise we fail
-
-            $repo_dir = $this->container->getParameter('melodycode_fossdroid.local_path_repo');
-            $metadata_dir = $this->container->getParameter('melodycode_fossdroid.local_path_metadata');
-            //$controller_file = $repo_dir.'/'.$app_controller_binary;
-            //$dongle_file	 = $repo_dir.'/'.$app_dongle_binary;
-
-            //if ( !file_exists($controller_file) /*|| !file_exists($dongle_file)*/ )
-            //{
-            //	$request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. Controller and/or Dongle binary file(s) do(es) not exist');
-            //	return $this->redirect($request->headers->get('referer'));
-            //
-            //}
-
-            // now we create the meta data file for the application
-            $metadata_file = $metadata_dir . '/' . $app_identifier . '.txt';
-
-            $content = 'License:Unknown' . PHP_EOL . 'Web Site:' . PHP_EOL . 'Source Code:' . PHP_EOL . 'Issue Tracker:' . PHP_EOL . 'Changelog:' . PHP_EOL . 'Summary:%s' . PHP_EOL . 'Description:' . PHP_EOL . '%s' . PHP_EOL . '.' . PHP_EOL . 'Name:%s' . PHP_EOL . 'Categories:%s' . PHP_EOL . '';
-            $content = sprintf($content, $app_summary, $app_description, $app_name, $app_category);
-
-            $success = file_put_contents($metadata_file, $content, LOCK_EX);
-
-            if (!$success) {
-                $request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. Unable to write metadata file');
-                return $this->redirect($request->headers->get('referer'));
-            }
-
-            $dbname = $this->container->getParameter('store_database_name');
-            $username = $this->container->getParameter('store_database_user');
-            $password = $this->container->getParameter('store_database_password');
-            $servername = $this->container->getParameter('store_database_host');
-
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-            // set the PDO error mode to exception
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            //$stmt = $conn->prepare('UPDATE Application SET ID =?,Version=?,Name=?,DongleAppName=?,ControllerAppName=?,Price=? WHERE ID = ? AND Version = ?');
-
-            $stmt = $conn->prepare('UPDATE Application SET ID =?, Name=?,Price=?, Type=? WHERE ID=?');
-            try {
-                //$stmt->execute([$app_identifier,$app_version,$app_name,$app_dongle_binary,$app_controller_binary,$app_price,$app_identifier,$app_version]);
-                $stmt->execute([$app_identifier, $app_name, $app_price, $app_type, $app_identifier]);
-
-            } catch (\PDOException $e) {
-                // if something goes wrong we fail
-                //throw $e;
-                $error = '';
-                if ($e->errorInfo[1] == 1062) // contrains violation i.e. the app_id:app_version already exists
-                {
-                    // Take some action if there is a key constraint violation, i.e. duplicate name
-                    $error = 'Operation Aborted .. The Application Identifier must be unique [Error]' . $e->getMessage();
-                } else
-                    $error = 'Operation Aborted ..' . $e->getMessage();
-
-                $request->getSession()->getFlashBag()->add('danger', $error);
-                return $this->redirect($request->headers->get('referer'));
-
-            }
-            // if we are here then all is good let us launch the update script
-            return $this->render('DashboardBundle:Developer:new-update-delete-app-result.html.twig', array('operation' => 'update'));
-        }
-
         $dbname = $this->container->getParameter('store_database_name');
         $username = $this->container->getParameter('store_database_user');
         $password = $this->container->getParameter('store_database_password');
@@ -301,51 +208,90 @@ class Developer1Controller extends Controller
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    }
+    public function editAppAction($slug)
+    {
+        $context = $this->container->get('security.context');
+        if (!$context->isGranted('IS_AUTHENTICATED_FULLY'))
+            return $this->render('DashboardBundle:Homepage:index.html.twig');
 
+        //if we are here user authenticated
+        // if we are here then the binary files are in place and the meta data file was created so we insert into the DB
+        $request = $this->get('request');
+        if ($request->getMethod() == 'POST') {
+            if ($this->scriptStillExecuting('update-store.sh')) {
+                //return new Response('<html><body><div>An Add Application Operation is in progress, please try again later</div> <div><input type="button" value="Try Again" onClick="window.history.back()"></div></body> </html>');
+                $request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. Another Add/Update/Delete Application operation is in progress .. Try again in a few seconds');
+                return $this->redirect($request->headers->get('referer'));
+            }
+
+            $app_name = $request->request->get('app-name');
+            $app_bundle = $request->request->get('app-bundle');
+            $app_description = $request->request->get('app-description');
+            $app_identifier = $request->request->get('app-identifier');
+            $app_payment_model = $request->request->get('app-payment-model');
+            $app_price = $request->request->get('app-price');
+            $app_summary = $request->request->get('app-summary');
+            $app_category = $request->request->get('app-category');
+            $app_type = $request->request->get('app-type');
+
+            // we check if the two binary files are where they should be other wise we fail
+            $repo_dir = $this->container->getParameter('melodycode_fossdroid.local_path_repo');
+            $metadata_dir = $this->container->getParameter('melodycode_fossdroid.local_path_metadata');
+            $metadata_file = $metadata_dir . '/' . $app_identifier . '.txt';
+            $content = 'License:Unknown' . PHP_EOL . 'Web Site:' . PHP_EOL . 'Source Code:' . PHP_EOL . 'Issue Tracker:' . PHP_EOL . 'Changelog:' . PHP_EOL . 'Summary:%s' . PHP_EOL . 'Description:' . PHP_EOL . '%s' . PHP_EOL . '.' . PHP_EOL . 'Name:%s' . PHP_EOL . 'Categories:%s' . PHP_EOL . '';
+            $content = sprintf($content, $app_summary, $app_description, $app_name, $app_category);
+            $success = file_put_contents($metadata_file, $content, LOCK_EX);
+
+            if (!$success) {
+                $request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. Unable to write metadata file');
+                return $this->redirect($request->headers->get('referer'));
+            }
+            $conn = $this->get_Store_DB_Object();
+            $stmt = $conn->prepare('UPDATE Application SET ID =?, Name=?,Price=?, Type=? WHERE ID=?');
+            try {
+                $stmt->execute([$app_identifier, $app_name, $app_price, $app_type, $app_identifier]);
+
+            } catch (\PDOException $e) {
+                if ($e->errorInfo[1] == 1062) // contrains violation i.e. the app_id:app_version already exists
+                {
+                    // Take some action if there is a key constraint violation, i.e. duplicate name
+                    $error = 'Operation Aborted .. The Application Identifier must be unique [Error]' . $e->getMessage();
+                } else {
+                    $error = 'Operation Aborted ..' . $e->getMessage();
+                }
+                $request->getSession()->getFlashBag()->add('danger', $error);
+                return $this->redirect($request->headers->get('referer'));
+            }
+            // if we are here then all is good let us launch the update script
+            return $this->render('DashboardBundle:Developer:new-update-delete-app-result.html.twig', array('operation' => 'update'));
+        }
+        $conn = $this->get_Store_DB_Object();
         $stmt = $conn->prepare('SELECT * FROM Application WHERE ID = ?');
         $stmt->execute([$slug]);
-
         $result = $stmt->fetchAll();
-
-        //$app_controller_binary = $result[0]['ControllerAppName'];
         $repo_dir = $this->container->getParameter('melodycode_fossdroid.local_path_repo');
         $metadata_dir = $this->container->getParameter('melodycode_fossdroid.local_path_metadata');
-
         // now we create the meta data file for the application
-
         $metadata_file = $metadata_dir . '/' . $slug . '.txt';
-
         if (!file_exists($metadata_file)) {
             $request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. metadata file:' . $metadata_file . ' does not exist');
             return $this->redirect($request->headers->get('referer'));
         }
 
         $content = file_get_contents($metadata_file);
-        //$content = 'License:Unknown'.PHP_EOL.'Web Site:'.PHP_EOL.'Source Code:'.PHP_EOL.'Issue Tracker:'.PHP_EOL.'Changelog:'.PHP_EOL.'Summary:%s'.PHP_EOL.'Description:'.PHP_EOL.'%s'.PHP_EOL.'.'.PHP_EOL.'Name:%s'.PHP_EOL.'Categories:%s'.PHP_EOL.'';
-        //$content = sprintf($content, $app_summary,$app_description,$app_name,$app_category);
-
         $summary = $this->get_string_between($content, 'Summary:', 'Description');
         $description = $this->get_string_between($content, 'Description:', 'Name:');
-
         $start = strpos($content, 'Categories:') + strlen('Categories:');
         $end = strlen($content);
         $appCategory = substr($content, $start, $end);
-
         $appCategory = trim($appCategory);
         $summary = trim($summary);
         $description = trim($description);
 
-        //echo '['.$summary.']';
-        //echo '['.$description.']';
-        //return new Response('<html><body> </body> </html>');
-
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
         $stmt = $conn->prepare('SELECT * FROM Category');
         $stmt->execute();
-
         $categories = $stmt->fetchAll();
 
         return $this->render('DashboardBundle:Developer:new-update-app-1.html.twig', array(
@@ -364,41 +310,17 @@ class Developer1Controller extends Controller
 
         if (!$context->isGranted('IS_AUTHENTICATED_FULLY'))
             return $this->render('DashboardBundle:Homepage:index.html.twig');
-//        //auth
-
+        //if we are here user authenticated
         $app_identifier = $slug;
         $request = $this->get('request');
-
         if ($this->scriptStillExecuting('update-store.sh')) {
-            //return new Response('<html><body><div>An Add Application Operation is in progress, please try again later</div> <div><input type="button" value="Try Again" onClick="window.history.back()"></div></body> </html>');
-            //return $this->render('DashboardBundle:Developer:new-app.html.twig');
             $request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. Another Add/Update/Delete Application operation is in progress .. Try again in a few seconds');
             return $this->redirect($request->headers->get('referer'));
         }
-//        if ( $this->scriptStillExecuting('update-store.sh'))
-//        {
-//            //return new Response('<html><body><div>An Add Application Operation is in progress, please try again later</div> <div><input type="button" value="Try Again" onClick="window.history.back()"></div></body> </html>');
-//            //return $this->render('DashboardBundle:Developer:new-app.html.twig');
-//            $request->getSession()->getFlashBag()->add('danger', 'Operation Aborted .. Another Add/Update/Delete Application operation is in progress .. Try again in a few seconds');
-//            return $this->redirect($request->headers->get('referer'));
-//        }
-
-
         if ($request->getMethod() == 'POST') {
-            $app_name = $request->request->get('app-name');
-            //$app_bundle				= $request->request->get('app-bundle');
-            $app_controller_binary = $request->request->get('app-binary');
-            //$app_description		= $request->request->get('app-description');
-            $app_dongle_binary = $request->request->get('app-binary');
             $app_identifier = $request->request->get('app-identifier');
-            //$app_payment_model	    = $request->request->get('app-payment-model');
-            //$app_price				= $request->request->get('app-price');
-            //$app_summary			= $request->request->get('app-summary');
             $app_version = $request->request->get('app-version');
-            //$app_category 			= $request->request->get('app-category');
-
             // we check if the two binary files are where they should be other wise we fail
-
             $repo_dir = $this->container->getParameter('melodycode_fossdroid.local_path_repo');
             $metadata_dir = $this->container->getParameter('melodycode_fossdroid.local_path_metadata');
             $target_dir = $repo_dir;
